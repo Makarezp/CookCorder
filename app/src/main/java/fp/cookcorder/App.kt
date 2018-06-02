1151d78a2a08e8d73437e28873bc9b254375f306
Filip Popiolek <fpopiolek@travelrepublic.co.uk>
2018-06-02 22:28:55 +0100

    added handling touch events

--- app/src/main/java/fp/cookcorder/App.kt
+++ app/src/main/java/fp/cookcorder/App.kt
@@ -3,8 +3,14 @@ package fp.cookcorder
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import fp.cookcorder.di.DaggerAppComponent
+import timber.log.Timber
 
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
--- app/src/main/java/fp/cookcorder/ui/main/MainFragment.kt
+++ app/src/main/java/fp/cookcorder/ui/main/MainFragment.kt
@@ -2,20 +2,24 @@ package fp.cookcorder.ui.main
 
 import android.Manifest.permission.RECORD_AUDIO
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v4.content.PermissionChecker.PERMISSION_GRANTED
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import fp.cookcorder.R
 import fp.cookcorder.extensions.onClick
 import kotlinx.android.synthetic.main.main_fragment.*
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.design.longSnackbar
+import timber.log.Timber
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), AnkoLogger {
 
     companion object {
 
@@ -34,23 +38,44 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(activity!!).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
         setupClicks()
     }
 
     private fun setupClicks() {
         mainFragmentFABRecord.onClick { requestRecordIfPermissionIsGranted() }
-
+        mainFragmentFABRecord.setOnTouchListener { v, m ->
+            when (m.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Timber.d("Action down")
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    Timber.d("Action Up")
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val rect = Rect()
+                    v.getHitRect(rect)
+                    if (rect.contains(v.left + m.x.toInt(), v.top + m.y.toInt())) {
+                        Timber.d("Inside")
+                    } else {
+                        Timber.d("Outside")
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
-    fun requestRecordIfPermissionIsGranted() {
+    private fun requestRecordIfPermissionIsGranted() {
 
         fun requestPermissions() {
             ActivityCompat.requestPermissions(activity!!, arrayOf(RECORD_AUDIO), RECORDING_PERMISSION_REQUEST)
         }
 
         if (ContextCompat.checkSelfPermission(context!!, RECORD_AUDIO) == PERMISSION_GRANTED) {
-           viewModel.requestNewRecord()
+            viewModel.requestNewRecord()
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!, RECORD_AUDIO))
                 longSnackbar(view!!,
@@ -62,7 +87,7 @@ class MainFragment : Fragment() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if(requestCode == RECORDING_PERMISSION_REQUEST) {
+        if (requestCode == RECORDING_PERMISSION_REQUEST) {
             permissions
                     .filterIndexed { index, s -> s == RECORD_AUDIO && grantResults[index] == PERMISSION_GRANTED }
                     .forEach { viewModel.requestNewRecord() }
