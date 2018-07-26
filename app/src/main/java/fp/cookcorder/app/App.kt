b55b6f20f1444f9b0274e194447995ffca080229
Filip Popiolek <fpopiolek@travelrepublic.co.uk>
2018-07-26 19:03:34 +0100

    added fabric

--- app/src/main/java/fp/cookcorder/app/App.kt
+++ app/src/main/java/fp/cookcorder/app/App.kt
@@ -1,15 +1,18 @@
 package fp.cookcorder.app
 
+import com.crashlytics.android.Crashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import fp.cookcorder.app.di.DaggerAppComponent
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class App : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         Timber.plant(Timber.DebugTree())
         AndroidThreeTen.init(this)
     }
