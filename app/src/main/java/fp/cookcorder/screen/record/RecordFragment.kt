2e732294e00f82c2c95dcfee87e570d0f6d221fe
Filip Popiolek <fpopiolek@travelrepublic.co.uk>
2018-08-01 20:54:26 +0100

    removed files after change of the design

--- app/src/main/java/fp/cookcorder/screen/record/RecordFragment.kt
+++ app/src/main/java/fp/cookcorder/screen/record/RecordFragment.kt
@@ -2,33 +2,18 @@ package fp.cookcorder.screen.record
 
 import android.Manifest
 import android.Manifest.permission.RECORD_AUDIO
-import android.app.Dialog
 import android.arch.lifecycle.ViewModelProviders
-import android.content.Context
-import android.content.DialogInterface
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v4.content.PermissionChecker
 import android.support.v4.content.PermissionChecker.PERMISSION_GRANTED
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.view.*
-import android.widget.Button
-import com.airbnb.epoxy.EpoxyViewHolder
-import com.airbnb.lottie.LottieAnimationView
 import dagger.android.support.DaggerFragment
 import fp.cookcorder.R
 import fp.cookcorder.app.ViewModelProviderFactory
 import fp.cookcorder.app.util.observe
-import fp.cookcorder.screen.utils.circularHide
-import fp.cookcorder.screen.utils.circularReval
-import kotlinx.android.synthetic.main.main_fragment.*
-import kotlinx.android.synthetic.main.record_dialog.*
 import org.jetbrains.anko.design.longSnackbar
-import timber.log.Timber
 import javax.inject.Inject
 
 class RecordFragment : DaggerFragment() {
@@ -47,10 +32,6 @@ class RecordFragment : DaggerFragment() {
 
     private lateinit var viewModel: RecordViewModel
 
-    private val dialogView: View by lazy { View.inflate(context, R.layout.record_dialog, null) }
-
-    private val dialog: Dialog by lazy { Dialog(context, R.style.AlertDialogStyle) }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
@@ -61,28 +42,15 @@ class RecordFragment : DaggerFragment() {
         viewModel = ViewModelProviders.of(activity!!, vmFactory).get(RecordViewModel::class.java)
         viewModel.permissionGranted = isPermissionGranted()
         if (!viewModel.permissionGranted) requestPermission()
-        prepareDialog()
         observeLiveData()
-        setupRecycler()
-        setupNumberPicker()
-    }
-
-    private fun setupNumberPicker() {
-
-    }
-
-    private fun setupRecycler() {
-        mainFragmentRV.layoutManager = GridLayoutManager(context, 3)
-        mainFragmentRV.adapter = viewModel.adapter
     }
 
     private fun observeLiveData() {
         observe(viewModel.isRecording) {
             if (it) {
-                showRecordDialog(viewModel.recordViewPosition!!)
+
             } else {
-                circularHide(dialogView)
-                dialog.dismiss()
+
             }
         }
         observe(viewModel.requestRecordingPermission) {
@@ -90,35 +58,6 @@ class RecordFragment : DaggerFragment() {
         }
     }
 
-    private fun prepareDialog() {
-        dialog.setContentView(dialogView)
-        dialogView.findViewById<Button>(R.id.button).setOnClickListener {
-            viewModel.finishRecording()
-        }
-        dialogView.findViewById<Button>(R.id.button2).setOnClickListener {
-            viewModel.cancelRecording()
-        }
-        dialog.setOnKeyListener(DialogInterface.OnKeyListener { dialogInterface, i, keyEvent ->
-            if (i == KeyEvent.KEYCODE_BACK) {
-
-                viewModel.cancelRecording()
-                return@OnKeyListener true
-            }
-            false
-        })
-
-        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-    }
-
-    private fun showRecordDialog(startRevalFrom: Pair<Int, Int>) {
-        dialog.setOnShowListener { circularReval(dialogView, startRevalFrom) }
-        dialog.show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        dialog.dismiss()
-    }
 
     private fun requestPermission() {
 
--- app/src/main/java/fp/cookcorder/screen/record/RecordViewModel.kt
+++ app/src/main/java/fp/cookcorder/screen/record/RecordViewModel.kt
@@ -6,15 +6,12 @@ import fp.cookcorder.screen.BaseViewModel
 import fp.cookcorder.screen.utils.SingleLiveEvent
 import fp.cookcorder.manager.TaskManager
 import io.reactivex.Observable
-import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
-import io.reactivex.subjects.Subject
 import timber.log.Timber
 import javax.inject.Inject
 
 class RecordViewModel @Inject constructor(
-        private val taskManager: TaskManager,
-        private val recordCellController: RecordCellController
+        private val taskManager: TaskManager
 ) : BaseViewModel() {
 
     val isRecording = MutableLiveData<Boolean>()
@@ -25,35 +22,9 @@ class RecordViewModel @Inject constructor(
 
     var blockStartingNewRecording = false
 
-    val adapter = recordCellController.adapter
-
-    var recordViewPosition: Pair<Int, Int>? = null
-
-    val successAtPosition = SingleLiveEvent<Int>()
-
-    var requestedMinutes: Int = 0
-
-    val successObservable: Observable<Int>
-    get() = successSubject
-
-    private val successSubject = PublishSubject.create<Int>()
-
-
-    @Inject
-    fun init() {
-        recordCellController.viewModel = this
-        recordCellController.setData(1.rangeTo(100).toList())
-    }
-
-    /**
-     * [x] view position at which record was requested
-     * [y] view position at which record was requested
-     */
-    fun requestNewRecord(minutes: Int, x: Int, y: Int) {
+    fun requestNewRecord(minutes: Int) {
         if (permissionGranted) {
             exe(taskManager.startRecordingNewTask(minutes.minutestToMilliseconds())) {
-                recordViewPosition = x to y
-                requestedMinutes = minutes
                 isRecording.value = true
             }
         } else requestRecordingPermission.call()
@@ -71,8 +42,6 @@ class RecordViewModel @Inject constructor(
             isRecording.value = false
         }) {
             isRecording.postValue(false)
-            successAtPosition.postValue(requestedMinutes)
-            successSubject.onNext(requestedMinutes)
         }
     }
 
